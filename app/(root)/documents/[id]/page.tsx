0a07eae8c9468f2621c3778dbd464c398c37d7c1
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2024-07-29 00:21:41 +0300

    Add text editor

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,8 +1,17 @@
+import Header from '@/components/Header'
+import { Editor } from '@/components/editor/Editor'
 import React from 'react'
 
 const Document = () => {
   return (
-    <div>Document</div>
+    <div>
+      <Header>
+        <div className='flex w-fit items-center justify-center gap-2'>
+          <p className='document-title'>This is a fake doc title</p>
+        </div>
+      </Header>
+      <Editor />
+    </div>
   )
 }
 
