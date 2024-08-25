6c32926fc7f8bc16457bd2bad8ceb9460f4af006
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2024-08-25 15:42:15 +0300

    Add Liveblocks structure

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,23 +1,9 @@
-import Header from '@/components/Header'
-import { Editor } from '@/components/editor/Editor'
-import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
-import React from 'react'
+
 
 const Document = () => {
   return (
     <div>
-      <Header>
-        <div className='flex w-fit items-center justify-center gap-2'>
-          <p className='document-title'>Share</p>
-        </div>
-        <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-      </Header>
-      <Editor />
+
     </div>
   )
 }
