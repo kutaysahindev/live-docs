2d784a01fda0b0e8bc0b62a7aaf93344d00ada04
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2024-09-01 22:07:43 +0300

    Add collaborative room

--- components/AddDocumentBtn.tsx
+++ components/AddDocumentBtn.tsx
@@ -3,10 +3,21 @@
 import React from 'react'
 import { Button } from './ui/button'
 import Image from 'next/image';
+import { createDocument } from '@/lib/actions/room.actions';
+import { useRouter } from 'next/navigation';
 
 const AddDocumentBtn = ({ userId, email}: AddDocumentBtnProps) => {
-  const addDocumentHandler = async () => {
+  const router = useRouter();
 
+  const addDocumentHandler = async () => {
+    try {
+      const room = await createDocument({ userId, email });
+      if(room){
+        router.push(`/documents/${room.id}`)
+      }
+    } catch (error) {
+      console.log(error)
+    }
   };
 
   return (
