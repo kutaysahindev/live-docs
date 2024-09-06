72ca44c4838206d708ce3b7bdaee8f7be4fd4f86
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2024-09-06 23:00:57 +0300

    Add editing logic for document title

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -5,15 +5,64 @@ import Header from '@/components/Header'
 import { Editor } from '@/components/editor/Editor'
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import ActiveCollaborators from './ActiveCollaborators';
+import { useRef, useState } from 'react';
+import { Input } from './ui/input';
+import Image from 'next/image';
 
 const CollaborativeRoom = ({roomId, roomMetadata}: CollaborativeRoomProps) => {
+
+  const currentUserType = "editor";
+
+  const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
+  const [editing, setEditing] = useState(false);
+  const [loading, setloading] = useState(false);
+  
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLDivElement>(null);
+
+  const updateTitleHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+
+  }
+
   return (
     <RoomProvider id={roomId}>
       <ClientSideSuspense fallback={<div>Loading…</div>}>
         <div className='collaborative-room'>
           <Header>
-            <div className='flex w-fit items-center justify-center gap-2'>
-              <p className='document-title'>Share</p>
+            <div ref={containerRef} className='flex w-fit items-center justify-center gap-2'>
+              {editing && !loading ? (
+                <Input
+                  type='text'
+                  value={documentTitle}
+                  ref={inputRef}
+                  placeholder='Enter title'
+                  onChange={(e) => setDocumentTitle(e.target.value)}
+                  onKeyDown={updateTitleHandler}
+                  disabled={!editing}
+                  className='document-title-input'
+                />
+              ) : (
+                <>
+                  <p className='document-title'>{documentTitle}</p>
+                </>
+              )}
+
+              {currentUserType === 'editor' && !editing && (
+                <Image 
+                  src="/assets/icons/edit.svg"
+                  alt='edit'
+                  width={24}
+                  height={24}
+                  onClick={() => setEditing(true)}
+                  className='pointer'
+                />
+              )}
+
+              {currentUserType !== 'editor' && !editing && (
+                <p className="view-only-tag">View only</p>
+              )}
+
+              {loading && <p className="text-sm text-gray-400">Saving...</p>}
             </div>
             <div className='flex w-full flex-1 justify-end gap-2 sm:gap-3'>
               <ActiveCollaborators />
